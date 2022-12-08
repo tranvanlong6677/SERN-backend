452c91651888c7055ab632cc726941da47f45c4e
“Tran <tranvanlong6677@gmail.com>
2022-12-08 22:20:45 +0700

    MVC desgin pattern - view and controller

--- src/configs/viewEngine.js
+++ src/configs/viewEngine.js
@@ -4,7 +4,8 @@ import express from "express";
 const configViewEngine = (app) => {
   app.use(express.static("../public"));
   app.set("view engine", "ejs");
-  app.set("views", "../views");
+  app.set("views", "./src/views");
+  // nên nhớ đường dẫn đến views phải bắt đầu từ src chứ ko phải tử thư mục viewEngine.js này
 };
 
 export default configViewEngine;
--- src/routes/web.js
+++ src/routes/web.js
@@ -1,12 +1,16 @@
 import express from "express";
+import homeController from "../controller/homeController";
 const router = express.Router();
 
 const initWebRoutes = (app) => {
-  router.get("/", (req, res) => {
-    return res.send("Hello world");
-  });
+  // router.get(path,handler)
+  // path là đường link
+  // handler là 1 function được thực thi khi router.get đến đường link chỉ định
+  router.get("/user", homeController.handleUserPage);
+  router.get("/", homeController.handleHelloWorld);
 
   return app.use("/", router);
+  // Ứng dụng bắt đầu với /
 };
 
 export default initWebRoutes;
